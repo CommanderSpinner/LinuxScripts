c9075abd1e84e9ffea23009800f54e8be43e8ef1
CommanderSpinner <123424514+CommanderSpinner@users.noreply.github.com>
2024-11-10 20:12:18 +0100

    Add files via upload

---
+++ config_files/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -Ux PATH $PATH ~/phpStorm/bin
+export PATH="$HOME/phpStorm/bin:$PATH"
+function fish_prompt
+    # Benutzername in Cyan
+    set_color yellow
+    echo -n (whoami)  # Zeigt den aktuellen Benutzername
+    
+    # PC-Name in Magenta
+    set_color yellow
+    echo -n "@"Debian  # Zeigt den Namen des PCs
+    
+    # Arbeitsverzeichnis in Gelb
+    set_color yellow
+    echo (prompt_pwd)  # Zeigt den aktuellen Arbeitsverzeichnis-Pfad
+    
+    # Pfeil-Symbol
+    set_color yellow
+    echo -n ">"
+    
+    # Zurücksetzen der Farben
+    set_color yellow
+end
+
+set -g fish_greeting ""
+fastfetch
+alias highlight="highlight -O ansi"
